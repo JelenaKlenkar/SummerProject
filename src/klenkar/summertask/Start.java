2e44c82c86bd8cc60da3b17b90773cd6818d990e
JelenaKlenkar <jelena.klenkar@gmail.com>
2019-08-03 22:03:10 +0200

    added menu in Start Class

--- src/klenkar/summertask/Start.java
+++ src/klenkar/summertask/Start.java
@@ -1,12 +1,116 @@
 package klenkar.summertask;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 public class Start {
+	private List<Applicant> applicants;
 
 	public Start() {
+		applicants = new ArrayList<>();
+		main_loop: while (true) {
+			menu();
+			switch (Helper.enterInteger("Enter number from menu")) {
+			case 1:
+				listApplicants();
+				break;
+			case 2:
+				applicants.add(enterApplicant());
+				break;
+			case 3:
+				editApplicant();
+				break;
+			case 4:
+				deleteApplicant();
+				break;
+			case 5:
+				break main_loop;
+			default:
+				break;
+
+			}
+		}
+
 	}
 
+	private void deleteApplicant() {
+		listApplicants();
+		Applicant a = applicants.get(ordinalNumberOfApplicant() - 1);
+		applicants.remove(a);
+
+	}
+
+	private void editApplicant() {
+		listApplicants();
+		Applicant a = applicants.get(ordinalNumberOfApplicant() - 1);
+		a = editValues(a);
+
+	}
+
+	private int ordinalNumberOfApplicant() {
+		int on;
+		while (true) {
+			on = Helper.enterInteger("Enter ordinal number");
+			if (on > applicants.size()) {
+				JOptionPane.showConfirmDialog(null, "Entering ordinal number required");
+				continue;
+			}
+			return on;
+		}
+	}
+
+	private Applicant enterApplicant() {
+
+		Applicant a = new Applicant();
+		a = setValues(a);
+		return a;
+
+	}
+
+	private void listApplicants() {
+		System.out.println("\nList all applicants");
+		int i = 0;
+		for (Applicant applicant : applicants) {
+			System.out.println(++i + "." + applicant);
+		}
+		System.out.println();
+	}
+
+	private void menu() {
+		System.out.println("====MENU SELECTION===");
+		System.out.println("1.List all the applicants");
+		System.out.println("2.Enter new applicant");
+		System.out.println("3. Edit applicant");
+		System.out.println("4.Delete applicant");
+		System.out.println("5.Exit the program");
+
+	}
+
+	private Applicant editValues(Applicant a) {
+		System.out.println("First name: " + a.getFirstName());
+		a.setFirstName(Helper.enterString("Enter first name: "));
+		System.out.println("Last name: " + a.getLastName());
+		a.setLastName(Helper.enterString("Enter last name: "));
+		System.out.println("Address: " + a.getLastName());
+		a.setAddress(Helper.enterString("Enter address: "));
+		System.out.println("Phone number: " + a.getPhoneNumber());
+		a.setPhoneNumber(Helper.enterInteger("Enter phone number: "));
+		System.out.println("Email address: " + a.getEmail());
+		a.setEmail(Helper.enterString("Enter email address: "));
+		System.out.println("Personal identification number:" + a.getPersonalIdentificationNumber());
+		a.setPersonalIdentificationNumber(Helper.enterInteger("Enter personal identification number: "));
+		System.out.println("Applicant cv: " + a.getApplicantCv());
+		a.setApplicantCv(Helper.enterString("Enter applicant cv: "));
+		System.out.println("Motivational letter: " + a.getMotivationalLetter());
+		a.setMotivationalLetter(Helper.enterString("Enter motivational letter: "));
+
+		return a;
+	}
+
+	// set the values for private fields in POJO class
 	private Applicant setValues(Applicant a) {
 		a.setFirstName(Helper.enterString("Enter first name: "));
 		a.setLastName(Helper.enterString("Enter last name: "));
